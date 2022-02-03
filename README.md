914a2957cfc728daaac7c273f3e30659356c92dc
chpoit <chpoit@chpoit.com>
2022-02-03 16:31:13 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,14 +1,15 @@
-# ESP-IDF + Arduino + Bluepad32 template app
+# snowcat-esp32-controller
 
-[![discord](https://img.shields.io/discord/775177861665521725.svg)](https://discord.gg/r5aMn6Cw5q)
+An esp32 based controller for the RCTF Snowcat
 
-![logo](https://gitlab.com/ricardoquesada/bluepad32-arduino/-/raw/main/img/bluepad32-arduino-logo.png)
+Project based on : https://gitlab.com/ricardoquesada/esp-idf-arduino-bluepad32-template
 
-This is a template application to be used with [Espressif IoT Development Framework](https://github.com/espressif/esp-idf).
 
-Please check [ESP-IDF docs](https://docs.espressif.com/projects/esp-idf/en/latest/get-started/index.html) for getting started instructions.
 
-Requires ESP-IDF **v4.4** or newer.
+
+# Building the firmware
+
+Requires ESP-IDF **v4.4** or newer (5.0 seems to fail).
 
 Includes the following ESP-IDF components, with a pre-configured `sdkconfig` file:
 
@@ -59,7 +60,7 @@ The following are the instructions for **Linux**, and should work for **macOS** 
     # Do it everytime you want to start a new project
     # Clone the template somewhere
     mkdir ~/src && cd ~/src
-    git clone --recursive https://gitlab.com/ricardoquesada/esp-idf-arduino-bluepad32-template.git my_project
+    git clone --recursive https://github.com/chpoit/snowcat-esp32-controller.git snowcat-esp32-controller
     ```
 
     Export the ESP-IDF environment variables in your shell:
@@ -74,116 +75,9 @@ The following are the instructions for **Linux**, and should work for **macOS** 
 
     ```sh
     # Compile it
-    cd ~/src/my_project
+    cd <PATH-TO>/snowcat-esp32-controller
     idf.py build
 
     # Flash + open debug terminal
-    idf.py flash monitor
+    idf.py flash
     ```
-
-[esp-idf-online-installer]: https://dl.espressif.com/dl/esp-idf/?idf=4.4
-[toolchain-deps]: https://docs.espressif.com/projects/esp-idf/en/latest/esp32/get-started/linux-setup.html
-[ttyusb0]: https://docs.espressif.com/projects/esp-idf/en/latest/esp32/get-started/establish-serial-connection.html#linux-dialout-group
-
-## Using 3rd party Arduino libraries
-
-To include 3rd party Arduino libraries in your project, you have to:
-
-* Add them to the `components` folder.
-* Add a file `component.mk` and/or `CMakeLists.txt` inside the component's folder
-
-`component.mk` is needed if you use `make` to compile it. And `CMakeLists.txt` is needed if you use `idf.py` to compile it.
-
-Let's use a real case as example:
-
-### Example: Adding ESP32Servo
-
-Suppose you want to use [ESP32Servo] project. The first thing to notice is that the source files are placed
-in the `src` folder.  We have to create a `component.mk` and/or `CMakeLists.txt` files that tells the
-ESP-IDF to look for the sources in the `src` folder.
-
-Example:
-
-```sh
-# 1) We clone ESP32Servo into components folder
-cd components
-git clone https://github.com/madhephaestus/ESP32Servo.git
-cd ESP32Servo
-```
-
-And now create create these files files inside `components/ESP32Servo` folder:
-
-```sh
-# 2) Create component.mk file
-# Only needed if you use "make" to compile the project
-# Copy & paste the following lines to the terminal:
-cat << EOF > component.mk
-COMPONENT_ADD_INCLUDEDIRS := src
-COMPONENT_SRCDIRS := src
-EOF
-```
-
-```sh
-# 3) Create CMakeLists.txt file
-# Only needed if you use "idf.py" to compile the project
-# Copy & paste the following lines to the terminal:
-cat << EOF > CMakeLists.txt
-idf_component_register(SRC_DIRS "src"
-                    INCLUDE_DIRS "src"
-                    REQUIRES "arduino")
-EOF
-```
-
-Finally, if you use `idf.py`, you have to update the dependencies in the `main/CMakeLists.txt`. E.g:
-
-```sh
-# Needed if you use "idf.py" to compile the project
-cd main
-edit CMakeLists.txt
-```
-
-...and append `ESP32Servo` to `REQUIRES`. The `main/CMakeLists.txt` should look like this:
-
-```cmake
-idf_component_register(SRCS "${srcs}"
-                    INCLUDE_DIRS "."
-                    REQUIRES "${requires}" "ESP32Servo")
-```
-
-And that's it. Now you can include `ESP32Servo` from your code. E.g:
-
-```cpp
-// Add this include in your arduino_main.cpp file
-#include <ESP32Servo.h>
-```
-
-[esp32servo]: https://github.com/madhephaestus/ESP32Servo.git
-
-## IDE
-
-Arduino IDE is not supported, but you can use [Visual Studio Code][vscode] + [ESP-IDF plugin][esp-idf-plugin].
-
-You can do:
-
-* All the regular Visual Studio Code regular features
-* ...plus configure, build, flash and monitor your project
-* ...and much more
-
-![ide](https://lh3.googleusercontent.com/pw/AM-JKLUxjqUhU2tM-bKw3togS3gTkBdtmi40kqW2c2KieAybnD770I3pdaLnFU7a-sM7dUUGmcWpigvElc1fGo1J-5bJlVdbg8HOJZKbUXo6A_IqIvUGEK6GtwxqNy5EFJmijrBnB_aQhd_fi3GQnXZ1V7WYvw=-no)
-
-Subjective opinion: VSCode + ESP-IDF plugin is muuuuuch better than Arduino IDE. Highly recommended!
-
-[vscode]: https://code.visualstudio.com/
-[esp-idf-plugin]: https://docs.espressif.com/projects/esp-idf/en/latest/esp32/get-started/vscode-setup.html
-
-## Further info
-
-* [Bluepad32 for Arduino](https://gitlab.com/ricardoquesada/bluepad32/-/blob/main/docs/plat_arduino.md)
-* [Arduino as ESP-IDF component](https://docs.espressif.com/projects/arduino-esp32/en/latest/esp-idf_component.html)
-* [ESP-IDF VSCode plugin](https://docs.espressif.com/projects/esp-idf/en/latest/esp32/get-started/vscode-setup.html)
-
-## Support
-
-* [Discord][discord]: any question? Ask them in our Discord server.
-
-[discord]: https://discord.gg/r5aMn6Cw5q
--- main/arduino_main.cpp
+++ main/arduino_main.cpp
@@ -19,17 +19,18 @@ limitations under the License.
 #include "sdkconfig.h"
 #ifndef CONFIG_BLUEPAD32_PLATFORM_ARDUINO
 #error "Must only be compiled when using Bluepad32 Arduino platform"
-#endif // !CONFIG_BLUEPAD32_PLATFORM_ARDUINO
+#endif  // !CONFIG_BLUEPAD32_PLATFORM_ARDUINO
 
 #include <Arduino.h>
 #include <Bluepad32.h>
+
+#define DEADZONE 20
 
 static GamepadPtr myGamepad;
 
 // This callback gets called any time a new gamepad is connected.
 // Up to 4 gamepads can be connected at the same time.
 void onConnectedGamepad(GamepadPtr gp) {
-    // In this example we only use one gamepad at the same time.
     myGamepad = gp;
     Serial.println("CALLBACK: Gamepad is connected!");
 }
@@ -51,79 +52,79 @@ void setup() {
     BP32.setup(&onConnectedGamepad, &onDisconnectedGamepad);
 }
 
+int scaleToESC(int axisValue) {
+    return 0;
+}
+
+int getSpeed(int axis, bool forward, bool backward, int throttleValue, int model) {
+    if (forward && backward) {
+        return 0;
+    }
+    if (forward) {
+        // Throttle reports as 0 for controllers without analog
+        if (model == Gamepad::CONTROLLER_TYPE_WiiController || model == Gamepad::CONTROLLER_TYPE_SwitchProController ||
+            model == Gamepad::CONTROLLER_TYPE_SwitchJoyConLeft || model == Gamepad::CONTROLLER_TYPE_SwitchJoyConRight ||
+            model == Gamepad::CONTROLLER_TYPE_SwitchJoyConPair) {
+            return 512;
+        }
+        return min(int(ceil(throttleValue / 2)), 512);
+    }
+    if (backward) {
+        return -511;
+    }
+    // Use this to account for
+    if (abs(axis) < DEADZONE) {
+        return 0;
+    }
+    // I just want forward to be positive and backwards to be negative, so I invert the values here
+    return -axis;
+}
+
+void handleThrottle(GamepadPtr gp) {
+    int leftSpeed = gp->axisY();
+    int rightSpeed = gp->axisRY();
+
+    int leftThrottle = gp->brake();
+    int rightThrottle = gp->throttle();
+
+    bool l1 = gp->l1();
+    bool l2 = gp->l2();
+    bool r1 = gp->r1();
+    bool r2 = gp->r2();
+
+    leftSpeed = getSpeed(leftSpeed, l2, l1, leftThrottle, gp->getModel());
+    rightSpeed = getSpeed(rightSpeed, r2, r1, rightThrottle, gp->getModel());
+
+    Serial.println("Left\t" + String(leftThrottle) + "(" + l2 + ")" + "\tRight\t" + String(rightThrottle) + "(" + r2 +
+                   ")");
+    Serial.println("LS\t" + String(leftSpeed) + "(" + l2 + ")" + "\tRS\t" + String(rightSpeed) + "(" + r2 + ")");
+}
+
 // Arduino loop function. Runs in CPU 1
 void loop() {
-    // This call fetches all the gamepad info from the NINA (ESP32) module.
-    // Just call this function in your main loop.
-    // The gamepads pointer (the ones received in the callbacks) gets updated
-    // automatically.
     BP32.update();
 
     // It is safe to always do this before using the gamepad API.
     // This guarantees that the gamepad is valid and connected.
     if (myGamepad && myGamepad->isConnected()) {
-        // There are different ways to query whether a button is pressed.
-        // By query each button individually:
-        //  a(), b(), x(), y(), l1(), etc...
-        if (myGamepad->a()) {
-            static int colorIdx = 0;
-            // Some gamepads like DS4 and DualSense support changing the color LED.
-            // It is possible to change it by calling:
-            switch (colorIdx % 3) {
-                case 0:
-                    // Red
-                    myGamepad->setColorLED(255, 0, 0);
-                    break;
-                case 1:
-                    // Green
-                    myGamepad->setColorLED(0, 255, 0);
-                    break;
-                case 2:
-                    // Blue
-                    myGamepad->setColorLED(0, 0, 255);
-                    break;
-            }
-            colorIdx++;
-        }
-
-        if (myGamepad->b()) {
-            // Turn on the 4 LED. Each bit represents one LED.
-            static int led = 0;
-            led++;
-            // Some gamepads like the DS3, DualSense, Nintendo Wii, Nintendo Switch
-            // support changing the "Player LEDs": those 4 LEDs that usually indicate
-            // the "gamepad seat".
-            // It is possible to change them by calling:
-            myGamepad->setPlayerLEDs(led & 0x0f);
-        }
-
-        if (myGamepad->x()) {
-            // Duration: 255 is ~2 seconds
-            // force: intensity
-            // Some gamepads like DS3, DS4, DualSense, Switch, Xbox One S support
-            // rumble.
-            // It is possible to set it by calling:
-            myGamepad->setRumble(0xc0 /* force */, 0xc0 /* duration */);
-        }
+        handleThrottle(myGamepad);
 
         // Another way to query the buttons, is by calling buttons(), or
         // miscButtons() which return a bitmask.
         // Some gamepads also have DPAD, axis and more.
-        char buffer[120];
-        snprintf(buffer, sizeof(buffer) - 1,
-                 "dpad: 0x%02x, buttons: 0x%04x, axis L: %4d, %4d, axis R: %4d, "
-                 "%4d, brake: %4d, throttle: %4d, misc: 0x%02x",
-                 myGamepad->dpad(),        // DPAD
-                 myGamepad->buttons(),     // bitmask of pressed buttons
-                 myGamepad->axisX(),       // (-511 - 512) left X Axis
-                 myGamepad->axisY(),       // (-511 - 512) left Y axis
-                 myGamepad->axisRX(),      // (-511 - 512) right X axis
-                 myGamepad->axisRY(),      // (-511 - 512) right Y axis
-                 myGamepad->brake(),       // (0 - 1023): brake button
-                 myGamepad->throttle(),    // (0 - 1023): throttle (AKA gas) button
-                 myGamepad->miscButtons()  // bitmak of pressed "misc" buttons
-        );
-        Serial.println(buffer);
+        // snprintf(buffer, sizeof(buffer) - 1,
+        //          "dpad: 0x%02x, buttons: 0x%04x, axis L: %4d, %4d, axis R: %4d, "
+        //          "%4d, brake: %4d, throttle: %4d, misc: 0x%02x",
+        //          myGamepad->dpad(),        // DPAD
+        //          myGamepad->buttons(),     // bitmask of pressed buttons
+        //          myGamepad->axisX(),       // (-511 - 512) left X Axis
+        //          myGamepad->axisY(),       // (-511 - 512) left Y axis
+        //          myGamepad->axisRX(),      // (-511 - 512) right X axis
+        //          myGamepad->axisRY(),      // (-511 - 512) right Y axis
+        //          myGamepad->brake(),       // (0 - 1023): brake button
+        //          myGamepad->throttle(),    // (0 - 1023): throttle (AKA gas) button
+        //          myGamepad->miscButtons()  // bitmak of pressed "misc" buttons
+        // );
 
         // You can query the axis and other properties as well. See Gamepad.h
         // For all the available functions.
