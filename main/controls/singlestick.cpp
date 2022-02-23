f301a9e3b57415881fd1f11db33cea3e774532f5
Charles Poitras <charlesp95@gmail.com>
2022-02-22 22:30:43 -0500

    messing around with stick while asleep

--- main/controls/singlestick.cpp
+++ main/controls/singlestick.cpp
@@ -2,23 +2,42 @@
 #include <Arduino.h>
 
 std::pair<double, double> SingleStick::handleThrottle(void) {
-    //todo: add edge of circle distance detection, otherwise it slows down while turning
+    // todo: add edge of circle distance detection, otherwise it slows down while turning
     double left = 0.;
     double right = 0.;
+    double forwards = 0.;
 
-    int yAxis = -gamepad->axisRY();
+    int yAxis = gamepad->axisRY();
     int xAxis = gamepad->axisRX();
-    yAxis = scaleRY(abs(yAxis) < deadzone ? 0 : yAxis);
-    xAxis = abs(xAxis) < deadzone * 1.5 ? 0 : xAxis;
+    yAxis = -scaleRY(abs(yAxis) < deadzone ? 0 : yAxis);
+    xAxis = scaleRX(abs(xAxis) < deadzone ? 0 : xAxis);
 
-    left = right = yAxis;
-
-    xAxis = scaleRX(xAxis);
-    if (xAxis < 0) {
-        left = left * max(1. - (xAxis / -511.), 0.);
-    } else {
-        right = right * max(1. - (xAxis / 512.), 0.);
+    forwards = yAxis + xAxis;
+    if (xAxis > 0 && yAxis > 0) {
+        forwards = yAxis + xAxis;
+        left = forwards;
+        right = forwards * max(1. - (xAxis / 512.), 0.);
+    } else if (xAxis > 0 && yAxis < 0) {
+        left = forwards;
+        right = forwards * max(1. - (xAxis / 512.), 0.);
+    } else if (xAxis < 0 && yAxis > 0) {
+        forwards = yAxis + xAxis;
+        left = forwards * max(1. - (xAxis / -511.), 0.);
+        right = forwards;
+    } else if (xAxis < 0 && yAxis < 0) {
+        left = forwards * max(1. - (xAxis / -511.), 0.);
+        right = forwards;
     }
+
+    // if (xAxis > 0) {
+    //     // right -= abs(xAxis);
+    //     left += xAxis;
+    //     right = right * max(1. - (xAxis / 512.), 0.);
+    // } else {
+    //     right += xAxis;
+    //     // left -= abs(xAxis);
+    //     left = left * max(1. - (xAxis / -511.), 0.);
+    // }
 
     return std::pair<double, double>(left, right);
 }
